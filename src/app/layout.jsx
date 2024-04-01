804ee0e6433cdad9487d12f34edcf74ec05161b3
manzolimatheus <manzoli.contato@gmail.com>
2024-04-01 00:56:45 -0300

    a

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,12 +12,18 @@ export const metadata = {
   title: "LoLo | Jogos para crianças",
   description: "Jogos para crianças",
   manifest: "/manifest.json",
+  viewport: "width=device-width, initial-scale=1, viewport-fit=cover",
+  other: {
+    mobileWebAppCapable: "yes",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body className={`${inter.className} overflow-hidden`}>
+      <body className={`${inter.className} overflow-hidden h-screen w-screen`}>
         <main>{children}</main>
       </body>
     </html>
